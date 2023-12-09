78bf08e3ade4fb72b2257c84eed46b40b2e61175
Hans Allendorfer <hans@alumni.purdue.edu>
2023-12-08 22:07:56 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hans Allendorfer on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
