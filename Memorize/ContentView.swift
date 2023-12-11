e723261a369870069114f5d81c35f924b8560bcd
Hans Allendorfer <hans@alumni.purdue.edu>
2023-12-10 16:08:37 -0800

    Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,33 +10,74 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let emojis: [String] = ["🐧","🐥","🐓","🐧", "🦚"]
-
+    let emojis: [String] = ["🐧","🐥","🐓","🐧", "🦚", "🦃", "🪿", "🦤", "🦩"]
+    @State var cardCount = 4
+    
     var body: some View {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in // index is an argument to the closure
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        // implicit return, this is not a view builder, just a normal function with one line of code, don't need an explicit return (same with computed properties)
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120, maximum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in // index is an argument to the closure
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    // internal vs external parameter names, by = label that callers use, offset = label that we use in the function
+    // symbol is both the internal and external parameter name
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = false // type inference, vars in structs require a value, ex) default value
+    @State var isFaceUp = true // type inference, vars in structs require a value, ex) default value
     
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12) // type inference, locals in @ViewBuilder
-            if isFaceUp {
+            Group {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2) // behaves like a View, behaves like a Shape
                 Text(content).font(.largeTitle)
-            } else {
-                base
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             print("Tapped")
             //isFaceUp = !isFaceUp // Views (self) including their vars are immutable, need to add @State
